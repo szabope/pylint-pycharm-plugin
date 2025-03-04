3b0444176f3b1327dc82d1d698f185039ab1172c
Peter Szabo <szabope@users.noreply.github.com>
2025-03-04 06:45:18 +0100

    fix open settings action

--- src/main/kotlin/works/szabope/plugins/pylint/action/OpenSettingsAction.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/action/OpenSettingsAction.kt
@@ -5,13 +5,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.ToolWindowManager
-import works.szabope.plugins.common.configurable.GeneralConfigurable
+import works.szabope.plugins.pylint.configurable.PylintConfigurable
 
 class OpenSettingsAction : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
         ToolWindowManager.getInstance(e.project ?: return).invokeLater {
             ShowSettingsUtil.getInstance().showSettingsDialog(
-                e.project, GeneralConfigurable::class.java
+                e.project, PylintConfigurable::class.java
             )
         }
     }
