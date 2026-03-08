bb3905de9fd323c162d987ea3979a4871b283438
Peter Szabo <szabope@users.noreply.github.com>
2026-03-08 05:00:36 +0100

    Fix test race that only shows up on CI runner

--- src/test/kotlin/works/szabope/plugins/pylint/action/ScanSdkTest.kt
+++ src/test/kotlin/works/szabope/plugins/pylint/action/ScanSdkTest.kt
@@ -49,7 +49,9 @@ class ScanSdkTest : AbstractToolWindowTestCase() {
             }
         }
         val target = TempFileSystem.getInstance().findFileByPath("/src")!!
-        scan(dataContext(project) { add(CommonDataKeys.VIRTUAL_FILE_ARRAY, arrayOf(target)) })
+        val context = dataContext(project) { add(CommonDataKeys.VIRTUAL_FILE_ARRAY, arrayOf(target)) }
+        waitForIt(ScanAction.ID, context)
+        scan(context)
         PlatformTestUtil.waitWhileBusy { PylintScanJobRegistryService.getInstance(project).isActive() }
         assertionError?.let { throw it }
         runBlocking {
