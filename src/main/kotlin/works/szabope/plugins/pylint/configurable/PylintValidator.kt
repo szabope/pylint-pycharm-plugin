58dcadc7505a11406d15dadebb5e6954c321f3ca
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 17:39:54 +0100

    refactor tool validation and its messages

--- src/main/kotlin/works/szabope/plugins/pylint/configurable/PylintValidator.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/configurable/PylintValidator.kt
@@ -2,17 +2,23 @@ package works.szabope.plugins.pylint.configurable
 
 import com.intellij.openapi.project.Project
 import works.szabope.plugins.common.validator.AbstractToolValidator
+import works.szabope.plugins.common.validator.ToolValidatorMessages
 import works.szabope.plugins.pylint.PylintBundle
 import works.szabope.plugins.pylint.services.PylintPluginPackageManagementService
 
-class PylintValidator(project: Project) : AbstractToolValidator(project) {
+class PylintValidator(project: Project) : AbstractToolValidator(project, MESSAGES) {
     override val versionFlag = "--version"
     override val packageName = "pylint"
     override fun getPackageManagementService() = PylintPluginPackageManagementService.getInstance(project)
-    override fun pathNotExistsMessage() = PylintBundle.message("pylint.configuration.path_to_executable.not_exists")
-    override fun pathIsDirectoryMessage() = PylintBundle.message("pylint.configuration.path_to_executable.is_directory")
-    override fun pathNotExecutableMessage() = PylintBundle.message("pylint.configuration.path_to_executable.not_executable")
-    override fun unknownVersionMessage() = PylintBundle.message("pylint.configuration.path_to_executable.unknown_version")
-    override fun invalidVersionMessage() = PylintBundle.message("pylint.configuration.pylint_invalid_version", PylintPluginPackageManagementService.MINIMUM_VERSION)
-    override fun notInstalledMessage() = PylintBundle.message("pylint.configuration.pylint_not_installed", "Pylint")
+
+    companion object {
+        private val MESSAGES = ToolValidatorMessages(
+            pathNotExists = PylintBundle.message("pylint.configuration.path_to_executable.not_exists"),
+            pathIsDirectory = PylintBundle.message("pylint.configuration.path_to_executable.is_directory"),
+            pathNotExecutable = PylintBundle.message("pylint.configuration.path_to_executable.not_executable"),
+            unknownVersion = PylintBundle.message("pylint.configuration.path_to_executable.unknown_version"),
+            invalidVersion = PylintBundle.message("pylint.configuration.pylint_invalid_version", PylintPluginPackageManagementService.MINIMUM_VERSION),
+            notInstalled = PylintBundle.message("pylint.configuration.pylint_not_installed", "Pylint")
+        )
+    }
 }
