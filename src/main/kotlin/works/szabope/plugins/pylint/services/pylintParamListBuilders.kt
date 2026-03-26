4534aae4716032f55d6fa7f398d33be9faae0d69
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 17:43:03 +0100

    fix misleading method name

--- src/main/kotlin/works/szabope/plugins/pylint/services/pylintParamListBuilders.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/services/pylintParamListBuilders.kt
@@ -4,13 +4,11 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.toCanonicalPath
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.virtualFile
-import com.intellij.platform.workspace.jps.entities.ExcludeUrlEntity
 import com.intellij.util.execution.ParametersListUtil
 import com.intellij.util.text.nullize
 import works.szabope.plugins.common.run.Exclusions
 import works.szabope.plugins.common.services.ToolExecutorConfiguration
 import works.szabope.plugins.pylint.PylintArgs
-import java.nio.file.Path
 import kotlin.io.path.Path
 
 context(project: Project)
@@ -22,15 +20,11 @@ fun buildParamList(
     arguments.nullize(true)?.let { params.addAll(ParametersListUtil.parse(it)) }
     if (excludeNonProjectFiles) {
         Exclusions(project).findAll(targets).mapNotNull {
-            getRelativePathFromContentRoot(it)?.toCanonicalPath()
+            it.url.virtualFile?.path?.let { p -> Path(p).toCanonicalPath() }
         }.joinToString(",").nullize()?.apply { params.add("--ignore-paths"); params.add(this) }
     }
     // mandatory args to take precedence
     params.addAll(PylintArgs.PYLINT_MANDATORY_COMMAND_ARGS.split(" "))
     targets.map { requireNotNull(it.canonicalPath) }.let { params.addAll(it) }
     params
-}
-
-private fun getRelativePathFromContentRoot(excludeUrlEntity: ExcludeUrlEntity): Path? {
-    return excludeUrlEntity.url.virtualFile?.path?.let { Path(it) }
 }
