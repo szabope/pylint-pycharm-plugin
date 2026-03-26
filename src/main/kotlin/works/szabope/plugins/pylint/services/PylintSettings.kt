c586b6e4382318dd4b16deb6e089e08a6bcba073
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 11:05:23 +0100

    refactor tool settings

--- src/main/kotlin/works/szabope/plugins/pylint/services/PylintSettings.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/services/PylintSettings.kt
@@ -2,22 +2,13 @@ package works.szabope.plugins.pylint.services
 
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.guessProjectDir
-import com.jetbrains.python.sdk.pythonSdk
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.annotations.TestOnly
 import works.szabope.plugins.common.blankToSingleSpace
-import works.szabope.plugins.common.services.BasicSettingsData
-import works.szabope.plugins.common.services.ToolExecutorConfiguration
-import works.szabope.plugins.common.services.Settings
-import works.szabope.plugins.common.services.ToolSettingsInvalidException
+import works.szabope.plugins.common.services.AbstractToolSettings
 
 @Service(Service.Level.PROJECT)
 @State(name = "PylintSettings", storages = [Storage("PylintPlugin.xml")], category = SettingsCategory.PLUGINS)
-class PylintSettings(internal val project: Project) :
-    SimplePersistentStateComponent<PylintSettings.PylintState>(PylintState()), Settings {
-
-    private var initialized = false
+class PylintSettings(private val project: Project) : AbstractToolSettings<PylintSettings.PylintState>(project, PylintState()) {
 
     @ApiStatus.Internal
     class PylintState : BaseState() {
@@ -33,9 +24,7 @@ class PylintSettings(internal val project: Project) :
 
     override var useProjectSdk
         get() = state.useProjectSdk
-        set(value) {
-            state.useProjectSdk = value
-        }
+        set(value) { state.useProjectSdk = value }
 
     override var executablePath
         get() = state.executablePath?.trim() ?: ""
@@ -51,83 +40,26 @@ class PylintSettings(internal val project: Project) :
 
     override var isAutoScrollToSource
         get() = state.autoScrollToSource
-        set(value) {
-            state.autoScrollToSource = value
-        }
+        set(value) { state.autoScrollToSource = value }
 
     override var excludeNonProjectFiles
         get() = state.excludeNonProjectFiles
-        set(value) {
-            state.excludeNonProjectFiles = value
-        }
+        set(value) { state.excludeNonProjectFiles = value }
 
     override var workingDirectory
         get() = state.projectDirectory
-        set(value) {
-            state.projectDirectory = value
-        }
+        set(value) { state.projectDirectory = value }
 
     override var scanBeforeCheckIn
         get() = state.scanBeforeCheckIn
-        set(value) {
-            state.scanBeforeCheckIn = value
-        }
+        set(value) { state.scanBeforeCheckIn = value }
 
-    override suspend fun initSettings(oldSettings: BasicSettingsData) {
-        if (state.executablePath == null) {
-            oldSettings.executablePath?.let { executablePath = it }
-        }
-        if (executablePath.isNotBlank() && project.pythonSdk == null) {
-            useProjectSdk = false
-        }
-        if (state.configFilePath == null) {
-            oldSettings.configFilePath?.let { configFilePath = it }
-        }
-        if (state.arguments == null) {
-            oldSettings.arguments?.let { arguments = it }
-        }
-        if (state.projectDirectory == null) {
-            workingDirectory = project.guessProjectDir()?.canonicalPath
-        }
-        initialized = true
-    }
-
-    override suspend fun getValidConfiguration(): Result<ToolExecutorConfiguration> {
-        val workingDirectory = workingDirectory
-        if (workingDirectory.isNullOrBlank()) {
-            return Result.failure(ToolSettingsInvalidException("Working directory is required"))
-        }
-        if (!isToolSet()) {
-            return Result.failure(ToolSettingsInvalidException("Pylint tool is not set"))
-        }
-
-        return ToolExecutorConfiguration(
-            executablePath,
-            useProjectSdk,
-            configFilePath,
-            arguments,
-            workingDirectory,
-            excludeNonProjectFiles,
-            scanBeforeCheckIn
-        ).let { Result.success(it) }
-    }
-
-    private suspend fun isToolSet(): Boolean {
-        return if (useProjectSdk) {
-            project.pythonSdk != null && PylintPluginPackageManagementService.getInstance(project)
-                .checkInstalledRequirement().isSuccess
-        } else {
-            executablePath.isNotBlank()
-        }
-    }
-
-
-    @TestOnly
-    fun reset() {
-        loadState(PylintState())
-    }
-
-    fun isInitialized() = initialized
+    override fun getPackageManagementService() = PylintPluginPackageManagementService.getInstance(project)
+    override fun toolNotSetMessage() = "Pylint tool is not set"
+    override fun isExecutableStateNull() = state.executablePath == null
+    override fun isConfigFileStateNull() = state.configFilePath == null
+    override fun isArgumentsStateNull() = state.arguments == null
+    override fun initialState() = PylintState()
 
     companion object {
         @JvmStatic
