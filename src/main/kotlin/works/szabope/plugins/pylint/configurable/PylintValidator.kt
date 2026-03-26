15c7811a7725280b7ace6e85faae5f480f917c7a
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 10:57:12 +0100

    refactor tool validation

--- src/main/kotlin/works/szabope/plugins/pylint/configurable/PylintValidator.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/configurable/PylintValidator.kt
@@ -1,80 +1,18 @@
 package works.szabope.plugins.pylint.configurable
 
-import com.intellij.execution.process.CapturingProcessHandler
-import com.intellij.execution.target.TargetedCommandLineBuilder
-import com.intellij.execution.target.local.LocalTargetEnvironment
-import com.intellij.execution.target.local.LocalTargetEnvironmentRequest
 import com.intellij.openapi.project.Project
-import com.jetbrains.python.packaging.PyPackage
-import works.szabope.plugins.common.services.PluginPackageManagementException
+import works.szabope.plugins.common.validator.AbstractToolValidator
 import works.szabope.plugins.pylint.PylintBundle
 import works.szabope.plugins.pylint.services.PylintPluginPackageManagementService
-import java.io.File
 
-class PylintValidator(private val project: Project) {
-    fun validateExecutablePath(path: String?): String? {
-        val path = path ?: return null
-        require(path.isNotBlank())
-        val file = File(path)
-        if (!file.exists()) {
-            return PylintBundle.message("pylint.configuration.path_to_executable.not_exists")
-        }
-        if (file.isDirectory) {
-            return PylintBundle.message("pylint.configuration.path_to_executable.is_directory")
-        }
-        if (!file.canExecute()) {
-            return PylintBundle.message("pylint.configuration.path_to_executable.not_executable")
-        }
-        return null
-    }
-
-    fun validateVersion(path: String): String? {
-        val pylintVersion = getVersionForExecutable(path)
-            ?: return PylintBundle.message("pylint.configuration.path_to_executable.unknown_version")
-        if (!PylintPluginPackageManagementService.getInstance(project).getRequirement()
-                .match(PyPackage("pylint", pylintVersion))
-        ) {
-            return PylintBundle.message(
-                "pylint.configuration.pylint_invalid_version", PylintPluginPackageManagementService.MINIMUM_VERSION
-            )
-        }
-
-        return null
-    }
-
-    suspend fun validateProjectSdk(): String? {
-        PylintPluginPackageManagementService.getInstance(project).checkInstalledRequirement().onFailure {
-            when (it) {
-                is PluginPackageManagementException.PackageNotInstalledException -> return PylintBundle.message(
-                    "pylint.configuration.pylint_not_installed", "Pylint"
-                )
-
-                is PluginPackageManagementException.PackageVersionObsoleteException -> return PylintBundle.message(
-                    "pylint.configuration.pylint_invalid_version", PylintPluginPackageManagementService.MINIMUM_VERSION
-                )
-            }
-        }
-        return null
-    }
-
-    private fun getVersionForExecutable(pathToExecutable: String): String? {
-        val targetEnvRequest = LocalTargetEnvironmentRequest()
-        val targetEnvironment = LocalTargetEnvironment(LocalTargetEnvironmentRequest())
-
-        @Suppress("UnstableApiUsage") val commandLineBuilder = TargetedCommandLineBuilder(targetEnvRequest)
-        commandLineBuilder.setExePath(pathToExecutable)
-        commandLineBuilder.addParameters("--version")
-
-        val targetCMD = commandLineBuilder.build()
-
-        val process = targetEnvironment.createProcess(targetCMD)
-
-        return runCatching {
-            @Suppress("UnstableApiUsage") val processHandler = CapturingProcessHandler(
-                process, targetCMD.charset, targetCMD.getCommandPresentation(targetEnvironment)
-            )
-            val processOutput = processHandler.runProcess(5000, true).stdout
-            "(\\d+.\\d+.\\d+)".toRegex().find(processOutput)?.groups?.last()?.value
-        }.getOrNull()
-    }
+class PylintValidator(project: Project) : AbstractToolValidator(project) {
+    override val versionFlag = "--version"
+    override val packageName = "pylint"
+    override fun getPackageManagementService() = PylintPluginPackageManagementService.getInstance(project)
+    override fun pathNotExistsMessage() = PylintBundle.message("pylint.configuration.path_to_executable.not_exists")
+    override fun pathIsDirectoryMessage() = PylintBundle.message("pylint.configuration.path_to_executable.is_directory")
+    override fun pathNotExecutableMessage() = PylintBundle.message("pylint.configuration.path_to_executable.not_executable")
+    override fun unknownVersionMessage() = PylintBundle.message("pylint.configuration.path_to_executable.unknown_version")
+    override fun invalidVersionMessage() = PylintBundle.message("pylint.configuration.pylint_invalid_version", PylintPluginPackageManagementService.MINIMUM_VERSION)
+    override fun notInstalledMessage() = PylintBundle.message("pylint.configuration.pylint_not_installed", "Pylint")
 }
