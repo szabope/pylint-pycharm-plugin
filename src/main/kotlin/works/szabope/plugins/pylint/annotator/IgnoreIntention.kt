d8b1e5933e762a304ba6ffe5a54791064e902019
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 17:22:34 +0100

    remove dead code

--- src/main/kotlin/works/szabope/plugins/pylint/annotator/IgnoreIntention.kt
+++
@@ -1,9 +0,0 @@
-package works.szabope.plugins.pylint.annotator
-
-/**
- * TODO: intention for inline suppression
- * Example: # pylint: disable=locally-disabled, multiple-statements, fixme, line-too-long
- * Precondition: algorithm for comment placement
- *  e.g. column == 0 doesn't always mean 'previous line', e.g. missing-final-newline
- */
-class IgnoreIntention
