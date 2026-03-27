3f04ac441893f66315ab03ef1c6bfd0f7b81aa92
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 11:01:24 +0100

    rename base class for ToolWindowFactory

--- src/main/kotlin/works/szabope/plugins/pylint/toolWindow/PylintToolWindowFactory.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/toolWindow/PylintToolWindowFactory.kt
@@ -1,9 +1,9 @@
 package works.szabope.plugins.pylint.toolWindow
 
 import com.intellij.openapi.project.Project
-import works.szabope.plugins.common.toolWindow.MyToolWindowFactory
+import works.szabope.plugins.common.toolWindow.AbstractToolWindowFactory
 import works.szabope.plugins.pylint.PylintBundle
 
-class PylintToolWindowFactory : MyToolWindowFactory(PylintBundle.message("pylint.toolwindow.name")) {
+class PylintToolWindowFactory : AbstractToolWindowFactory(PylintBundle.message("pylint.toolwindow.name")) {
     override fun createPanel(project: Project) = PylintToolWindowPanel(project)
 }
