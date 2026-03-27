5ae324dc5f35f5f08a545606782cce38d437b2fa
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 11:27:07 +0100

    simplify RescanAction

--- src/main/kotlin/works/szabope/plugins/pylint/action/RescanAction.kt
+++ src/main/kotlin/works/szabope/plugins/pylint/action/RescanAction.kt
@@ -2,12 +2,12 @@ package works.szabope.plugins.pylint.action
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.vfs.VirtualFile
-import works.szabope.plugins.pylint.toolWindow.PylintTreeService
 
 class RescanAction : ScanAction() {
 
-    override fun listTargets(event: AnActionEvent): Collection<VirtualFile> {
-        return PylintTreeService.getInstance(event.project ?: return emptyList()).getRootScanPaths()
+    override fun listTargets(event: AnActionEvent): Collection<VirtualFile>? {
+        val project = event.project ?: return null
+        return getTreeService(project).getRootScanPaths()
     }
 
     companion object {
